c20cced822e77710fcef1b99f4a4308dfd5fe96d
novoondr-stack <novoondr@artx.cz>
2026-03-01 16:30:00 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
-  turbopack: {
-    root: __dirname,
-  },
-}
-
-module.exports = nextConfig
